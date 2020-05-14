ba3cc38d391a22f8808d644ee73208f1548b12fa
Aaron Kofsky <aaronko@umich.edu>
2020-05-14 02:22:20 -0400

    Implement basic movement
    oh man i really should test this stuff shuoldn't i

--- src/board.rs
+++ src/board.rs
@@ -128,57 +128,24 @@ impl Board {
         if start_piece.color != player {
             return Err("You aren't Caliborn");
         }
+        let valid_end_spots: Vec<(i8, i8)> = match start_piece.piece {
+            Pawn => check_pawn(self, start, player),
+            Knight => check_knight(self, start, player),
+            Bishop => check_bishop(self, start, player),
+            Rook => check_rook(self, start, player),
+            Queen => check_queen(self, start, player),
+            King => check_king(self, start, player),
+        };
 
-        // Pieces can move in two main ways
-        // First, pieces may move onto Empty squares as long as all of the
-        // intermediate squares are empty (except for the knight, which does not
-        // require this)
-        // Alternatively, pieces may move onto another piece of othe opposite color
-        // so long as it captures the piece.
-        // Pawns can move either forwards, or diagonally forwards
-        match start_piece.piece {
-            Pawn => {
-                // TODO: first time pawn movement
-                // a pawn can move directly forward if moving into empty space
-                let valid_move_spot = (start.0, start.1 + player.direction());
-                let valid_capture_spot1 = (start.0 + 1, start.1 + player.direction());
-                let valid_capture_spot2 = (start.0 - 1, start.1 + player.direction());
-                if end == valid_move_spot {
-                    if end_piece == None {
-                        return Ok(());
-                    } else {
-                        return Err("Can't move pawn into occupied space this way!");
-                    }
-                }
-                // a pawn can move diagonally if capturing
-                else if end == valid_capture_spot1 || end == valid_capture_spot2 {
-                    if let Some(end_piece) = end_piece {
-                        if end_piece.color != player {
-                            return Ok(());
-                        } else {
-                            return Err("Can't move diagonally unless it's a capture");
-                        }
-                    } else {
-                        return Err("Can't move diagonally unless it's a capture");
-                    }
-                } else {
-                    println!(
-                        "Valid pawn moves are: {:?} {:?} {:?}",
-                        valid_move_spot, valid_capture_spot1, valid_capture_spot2
-                    );
-                    return Err("That's not a valid pawn move");
-                }
-            }
-            Knight => unimplemented!(),
-            Bishop => unimplemented!(),
-            Rook => unimplemented!(),
-            Queen => unimplemented!(),
-            King => unimplemented!(),
+        if valid_end_spots.contains(&end) {
+            Ok(())
+        } else {
+            Err("Can't move a piece there")
         }
     }
 
     /// Gets the piece located at the coordinates
-    fn get(&self, coord: (i8, i8)) -> Option<Piece> {
+    pub fn get(&self, coord: (i8, i8)) -> Option<Piece> {
         // i promise very very hard that this i8 is, in fact, in the range 0-7
         self.board[(7 - coord.1) as usize][coord.0 as usize]
     }
@@ -188,6 +155,137 @@ impl Board {
         // i promise very very hard that this i8 is, in fact, in the range 0-7
         self.board[(7 - coord.1) as usize][coord.0 as usize] = piece;
     }
+}
+
+fn check_pawn(board: &Board, pos: (i8, i8), color: Color) -> Vec<(i8, i8)> {
+    let mut valid_end_pos = Vec::new();
+    let forwards = (pos.0, pos.1 + color.direction());
+    if on_board(forwards) {
+        if board.get(forwards).is_none() {
+            valid_end_pos.push(forwards);
+        }
+    }
+
+    for &diagonal in &[
+        (pos.0 + 1, pos.1 + color.direction()),
+        (pos.0 - 1, pos.1 + color.direction()),
+    ] {
+        if on_board(diagonal) {
+            match board.get(diagonal) {
+                Some(piece) if piece.color != color => valid_end_pos.push(diagonal),
+                _ => {}
+            }
+        }
+    }
+    valid_end_pos
+}
+
+fn on_board(pos: (i8, i8)) -> bool {
+    0 <= pos.0 && pos.0 < 8 && 0 <= pos.1 && pos.1 < 8
+}
+
+fn check_knight(board: &Board, pos: (i8, i8), color: Color) -> Vec<(i8, i8)> {
+    let mut valid_end_pos = Vec::new();
+    let deltas = get_move_deltas(PieceType::Knight).unwrap();
+    for delta in deltas {
+        let end_pos = (pos.0 + delta.0, pos.1 + delta.1 * color.direction());
+        if !on_board(end_pos) {
+            continue;
+        }
+        match board.get(end_pos) {
+            Some(piece) if piece.color != color => valid_end_pos.push(end_pos),
+            None => valid_end_pos.push(end_pos),
+            _ => {}
+        }
+    }
+    valid_end_pos
+}
+fn check_bishop(board: &Board, pos: (i8, i8), color: Color) -> Vec<(i8, i8)> {
+    // from the bishop's position, scan in each cardinal direction until you either
+    // hit the end of the board, or hit a piece
+    // if the color of the piece matches `color`, then don't include that space
+    // otherwise, do include it (you can capture the spot)
+    let mut valid_end_pos = Vec::new();
+    let line_of_sights: Vec<Vec<(i8, i8)>> = get_los_bishop(pos);
+    for los in line_of_sights {
+        for end_pos in los {
+            let end_piece = board.get(end_pos);
+            if end_piece == None {
+                valid_end_pos.push(end_pos);
+            } else if let Some(piece) = end_piece {
+                if piece.color != color {
+                    valid_end_pos.push(end_pos);
+                }
+                break;
+            }
+        }
+    }
+
+    valid_end_pos
+}
+
+fn check_rook(board: &Board, pos: (i8, i8), color: Color) -> Vec<(i8, i8)> {
+    // from the rook's position, scan in each cardinal direction until you either
+    // hit the end of the board, or hit a piece
+    // if the color of the piece matches `color`, then don't include that space
+    // otherwise, do include it (you can capture the spot)
+    let mut valid_end_pos = Vec::new();
+    let line_of_sights: Vec<Vec<(i8, i8)>> = get_los_rook(pos);
+    for los in line_of_sights {
+        for end_pos in los {
+            let end_piece = board.get(end_pos);
+            if end_piece == None {
+                valid_end_pos.push(end_pos);
+            } else if let Some(piece) = end_piece {
+                if piece.color != color {
+                    valid_end_pos.push(end_pos);
+                }
+                break;
+            }
+        }
+    }
+
+    valid_end_pos
+}
+
+fn check_queen(board: &Board, pos: (i8, i8), color: Color) -> Vec<(i8, i8)> {
+    // from the queen's position, scan in each cardinal direction until you either
+    // hit the end of the board, or hit a piece
+    // if the color of the piece matches `color`, then don't include that space
+    // otherwise, do include it (you can capture the spot)
+    let mut valid_end_pos = Vec::new();
+    let line_of_sights: Vec<Vec<(i8, i8)>> = [get_los_bishop(pos), get_los_rook(pos)].concat();
+    for los in line_of_sights {
+        for end_pos in los {
+            let end_piece = board.get(end_pos);
+            if end_piece == None {
+                valid_end_pos.push(end_pos);
+            } else if let Some(piece) = end_piece {
+                if piece.color != color {
+                    valid_end_pos.push(end_pos);
+                }
+                break;
+            }
+        }
+    }
+
+    valid_end_pos
+}
+fn check_king(board: &Board, pos: (i8, i8), color: Color) -> Vec<(i8, i8)> {
+    let mut valid_end_pos = Vec::new();
+    let deltas = get_move_deltas(PieceType::King).unwrap();
+    for delta in deltas {
+        let end_pos = (pos.0 + delta.0, pos.1 + delta.1 * color.direction());
+        if !on_board(end_pos) {
+            continue;
+        }
+        match board.get(end_pos) {
+            Some(piece) if piece.color != color => valid_end_pos.push(end_pos),
+            None => valid_end_pos.push(end_pos),
+            _ => {}
+        }
+    }
+    valid_end_pos
 }
 
 impl fmt::Display for Board {
@@ -207,6 +305,52 @@ impl fmt::Display for Board {
     }
 }
 
+fn get_los_rook(pos: (i8, i8)) -> Vec<Vec<(i8, i8)>> {
+    let mut los_right = Vec::new();
+    for i in pos.0 + 1..8 {
+        los_right.push((i, pos.1));
+    }
+
+    let mut los_left = Vec::new();
+    for i in 0..pos.0 {
+        los_left.push((i, pos.1));
+    }
+
+    let mut los_up = Vec::new();
+    for i in pos.1 + 1..8 {
+        los_up.push((pos.0, i));
+    }
+
+    let mut los_down = Vec::new();
+    for i in 0..pos.1 {
+        los_down.push((pos.0, i));
+    }
+    return vec![los_up, los_down, los_right, los_left];
+}
+
+fn get_los_bishop(pos: (i8, i8)) -> Vec<Vec<(i8, i8)>> {
+    let mut los_up_right = Vec::new();
+    for (i, j) in (pos.0 + 1..8).zip(pos.1 + 1..8) {
+        los_up_right.push((i, j));
+    }
+
+    let mut los_down_left = Vec::new();
+    for (i, j) in (0..pos.0).zip(0..pos.1) {
+        los_down_left.push((i, j));
+    }
+
+    let mut los_up_left = Vec::new();
+    for (i, j) in (0..pos.0).zip(pos.1 + 1..8) {
+        los_up_left.push((i, j));
+    }
+
+    let mut los_down_right = Vec::new();
+    for (i, j) in (pos.0 + 1..8).zip(0..pos.1) {
+        los_down_right.push((i, j));
+    }
+    // TODO finish this function
+    return vec![los_up_right, los_up_left, los_down_right, los_down_left];
+}
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Piece {
     color: Color,
@@ -250,9 +394,9 @@ pub enum PieceType {
 /// Return a list of valid movement deltas (offsets from the piece) given a
 /// PieceType. Move deltas take into account the piece's color.
 /// This function returns Err on Rook, Bishop, and Queen
-fn get_move_deltas(piece: Piece) -> Result<Vec<(i8, i8)>, &'static str> {
+fn get_move_deltas(piece: PieceType) -> Result<Vec<(i8, i8)>, &'static str> {
     use PieceType::*;
-    match piece.piece {
+    match piece {
         Pawn => Ok(vec![(0, 1)]),
         Knight => Ok(vec![
             (1, 2),
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,30 @@
 mod board;
 
 use board::*;
-use std::fmt;
+use std::io;
 
-fn main() -> Result<(), &'static str> {
+pub fn read_string_from_stdin(message: Option<String>) -> String {
+    if let Some(x) = message {
+        println!("{}", x);
+    }
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    input = input.trim().to_string(); // Remove trailing newline
+    println!("asdf: {}", input);
+    input
+}
+
+fn main() {
     let mut board = Board::default();
-    println!("{}", board);
-
-    println!("moved a white pawn!");
-    board
-        .move_piece(Color::White, (0, 1), (0, 2))
-        .expect("oh god oh fuck it broke");
-    println!("{}", board);
-
-    println!("captured a white pawn with a black pawn!");
-    board.move_piece(Color::Black, (3, 6), (3, 5))?;
-    board.move_piece(Color::Black, (3, 5), (3, 4))?;
-    board.move_piece(Color::Black, (3, 4), (3, 3))?;
-    board.move_piece(Color::Black, (3, 3), (3, 2))?;
-    board.move_piece(Color::Black, (3, 2), (4, 1))?;
-    println!("{}", board);
-
-    return Ok(());
+    loop {
+        println!("{}", board);
+        let a: i8 = read_string_from_stdin(None).parse().unwrap();
+        let b: i8 = read_string_from_stdin(None).parse().unwrap();
+        println!("Chosen piece: {:?}", board.get((a, b)));
+        let c: i8 = read_string_from_stdin(None).parse().unwrap();
+        let d: i8 = read_string_from_stdin(None).parse().unwrap();
+        println!("Goal place: {:?}", board.get((c, d)));
+        let test: i8 = "1".to_string().parse().unwrap();
+        println!("{:?}", board.move_piece(Color::White, (a, b), (c, d)));
+    }
 }
